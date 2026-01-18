d9b82fca8c7dcd0bfd564b29f4e7b387b6798f0e
Islam412 <islam.124.hamdy@gmail.com>
2026-01-18 11:42:50 +0200

    desing profile models

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -26,3 +26,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return str(self.username)
+    
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cover_images = models.ImageField(_('Cover Image'), upload_to='Images_Profile', null=True, blank=True, default='user.png')
+    address = models.TextField(_('Address'), max_length=300 , null=True, blank=True, on_delete=models.SET_NULL)
+    phone = phone = models.CharField(_('Phone'), max_length=30, null=True, blank=True, on_delete=models.SET_NULL)
+    code = models.CharField(max_length=10, default=generate_code)
+    verified = models.BooleanField(_('Verified'), default=False)
+
+    @property
+    def full_name(self):
+        return f"{self.user.first_name} {self.user.last_name}" if self.user.first_name and self.user.last_name else self.user.username
+
+    @property
+    def email(self):
+        return self.user.email
+
+    @property
+    def username(self):
+        return self.user.username
+    
+
+    
