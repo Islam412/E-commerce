80bb26cdcc39f3021e1d863e1b99cc360b9cb83e
Islam412 <islam.124.hamdy@gmail.com>
2026-01-18 11:29:17 +0200

    desing user model

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -13,3 +13,16 @@ import uuid
 from utils.generate_code import generate_code
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(_('Frist Name'),max_length=255, null=True, blank=True)
+    last_name = models.CharField(_('Last Name'),max_length=255, null=True, blank=True)
+    username = models.CharField(_('Username'),max_length=255, unique=True, null=True, blank=True)
+    email = models.EmailField(_('Email'),unique=True)
+
+    # Change defult django in adminbanal
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return str(self.username)
