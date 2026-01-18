b0291753358acd69fcbc1d1f9d1c57c1acc41de7
Islam412 <islam.124.hamdy@gmail.com>
2026-01-18 17:25:24 +0200

    desing profule create api views

--- backend/userauths/api.py
+++ backend/userauths/api.py
@@ -28,3 +28,7 @@ class ProfileListAPIView(ListAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
+
+
+class ProfileCreateAPIView(CreateAPIView):
+    serializer_class = ProfileSerializer
