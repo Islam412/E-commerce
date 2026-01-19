90d004e6cd8f61b86d19dc294436cf0c95606d1b
Islam412 <islam.124.hamdy@gmail.com>
2026-01-19 17:16:41 +0200

    create my account api urls

--- backend/userauths/urls.py
+++ backend/userauths/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('api/profile/', api.ProfileListAPIView.as_view(), name='profile_api_list'),
     path('api/profile/create/', api.ProfileCreateAPIView.as_view(), name='profile_api_create'),
     path('api/profile/update/<pk>/', api.UpdateMyProfileAPIView.as_view(), name='profile_api_update'),
+    path('api/account/me/', api.MyAccountAPIView.as_view(), name='my_account'),
 ]
