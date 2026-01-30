0015e6101eed6deb8ced92da7bde528a5808514f
Islam412 <islam.124.hamdy@gmail.com>
2026-01-30 20:11:47 +0200

    desing product detail api urls

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .api import ProductListAPI
+from .api import ProductListAPI , ProductDetailAPI
 
 
 app_name = 'products'
@@ -8,4 +8,5 @@ app_name = 'products'
 
 urlpatterns = [
     path('api/genariclist', ProductListAPI.as_view()),
+    path('api/genariclist/<int:pk>', ProductDetailAPI.as_view()),
 ]
