f9d4c7ef700f49e5be598d61778844b169397ef8
Islam412 <islam.124.hamdy@gmail.com>
2026-01-30 15:11:35 +0200

    desing product image tabulare admin

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, ProductImage,Brand,Review
 
 # Register your models here.
+
+# admin many image of product in same product
+class ProductImageTabular(admin.TabularInline):
+    model = ProductImage
