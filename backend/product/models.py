fb54942657538b25d5f32ab1bf132fc7e28ff291
Islam412 <islam.124.hamdy@gmail.com>
2026-01-30 14:49:29 +0200

    create product models p2

--- backend/product/models.py
+++ backend/product/models.py
@@ -32,3 +32,24 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    # instance methods = each object , self=object
+    def avg_rate(self):
+        avg = self.review_product.aggregate(rate_avg=Avg('rate'))
+        if not avg['rate_avg']:
+            return 0
+            return result
+        return avg['rate_avg']
+    
+    def save(self, *args, **kwargs):
+       self.slug = slugify(self.name)
+       super(Product, self).save(*args, **kwargs)
+       
+    @property
+    def average_rating(self):
+        reviews = self.review_product.all()
+        if reviews.count() > 0:
+            total_rating = sum(review.rate for review in reviews)
+            return total_rating / reviews.count()
+        return 0  
+
