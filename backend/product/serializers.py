737ce97756bf9df634a506cfec7eec4f61f10ef9
Islam412 <islam.124.hamdy@gmail.com>
2026-01-30 15:21:41 +0200

    desing brand details serializer

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -60,3 +60,11 @@ class ProductDetailSerializers(TaggitSerializer,serializers.ModelSerializer):
     def get_review_count(self, product:Product):
         review = product.review_product.all().count()
         return review
+    
+
+
+class BrandDetailSerializers(serializers.ModelSerializer):
+    products = ProductListSerializers(source='product_name', many=True)
+    class Meta:
+        model = Brand
+        fields = '__all__'
